6f5c7c3c0c9dc4aad41d702d181b8de128a62104
varuninduri <45063946+varuninduri@users.noreply.github.com>
2018-11-27 20:34:58 +0530

    Update db.sh

--- db.sh
+++ db.sh
@@ -11,3 +11,24 @@ if [ "" == "$MONGO_INSTALLED" ]; then
   apt-get update
   apt-get install -y mongodb-org
 fi
+GIT="git"
+# Prerequisites
+GIT_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' $GIT | grep "install ok installed")
+echo "Checking for $GIT: $GIT_INSTALLED"
+if [ "" == "$GIT_INSTALLED" ]; then
+  apt-get update
+  apt-get install -y $GIT
+fi
+#!/bin/sh
+echo "cloning code from git repo"
+cd /home/vagrant
+git clone https://github.com/varuninduri/Vagrant.git
+sleep 30s
+echo "cloned"
+cd /home/vagrant/Vagrant
+sudo rm -rf /etc/mongod.conf
+echo "conf removed"
+sudo cp mongod.conf /etc/
+echo "conf copied"
+sudo service mongod restart
+echo "mongod restarted"
